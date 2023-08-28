f391759c8c3d45bbac1e1019857beb7885e2bff3
tekelala <camilo@tekelala.com>
2023-08-28 11:49:03 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
 def prompt_compliance(text_file):
-    prompts = f'''Role: You are a top data analyst read and carefully find the information, then extract and present information from {text_file}. Do the following tasks and answer always in Spanish.
-    Task 1: Identify the "Razón Social"
-    Task 2: Identify the "Nit"
-    Task 3: Identify the "Domicilio principal"
-    Task 4: Identify the "Matrícula No."
-    Task 5: Identify the "Teléfono comercial 1:"
-    Task 6: Identify the "Teléfono comercial 2:" 
-    Task 7: Identify the "Página web:"
-    Task 8: Identify the "Correo electrónico:"
-    Task 9: Identify the "OBJETO SOCIAL:"
-    Task 10: Identify the "REPRESENTANTE LEGAL PRINCIPAL"
-    Task 11: Identify the "REPRESENTANTE LEGAL SUPLENTE"
-    Task 12: Identify the "Actividad principal código CIIU:"
-    Task 13: Identify the "Actividad secundaria código CIIU:"
+    prompts = f'''Role: You are a top data analyst read and carefully find the information, then extract and present information from {text_file}. It does not matter if the information is with upper or lower letters. The key is to find the relevant information. Do the following tasks and answer always in Spanish.
+    Task 1: Search for the "Razón Social"
+    Task 2: Search for the "Nit"
+    Task 3: Search for the "Domicilio principal"
+    Task 4: Search for the "Matrícula No."
+    Task 5: Search for the "Teléfono comercial 1:"
+    Task 6: Search for the "Teléfono comercial 2:" 
+    Task 7: Search for the "Página web:"
+    Task 8: Search for the "Correo electrónico:"
+    Task 9: Search for the "OBJETO SOCIAL:"
+    Task 10: Search for the "REPRESENTANTE LEGAL PRINCIPAL"
+    Task 11: Search for the "REPRESENTANTE LEGAL SUPLENTE"
+    Task 12: Search for the "Actividad principal Código CIIU:"
+    Task 13: Search for the "Actividad secundaria Código CIIU:"
     
     Here is an example of the information you will be asked to provide:    
     Razón Social: UNERGY S.A.S.
